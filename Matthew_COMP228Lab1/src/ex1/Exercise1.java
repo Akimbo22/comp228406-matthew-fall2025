57ed13c3b6ca872de406e6f12f056a069452ff22
Matthew Elliott <matthewe700@gmail.com>
2025-09-19 04:51:33 -0400

    Lab 1 Code Completed
    Adding the completed Lab 1 code for submission

--- Matthew_COMP228Lab1/src/ex1/Exercise1.java
+++ Matthew_COMP228Lab1/src/ex1/Exercise1.java
@@ -1,4 +1,44 @@
+//Matthew Elliott - 301424215
+//September 18, 2025
+// Lab #1
+
 package ex1;
 
 public class Exercise1 {
+    // Variables:
+    private int id;
+    private String name;
+    private String address;
+    private String dob;
+    private int numAlbums;
+
+    // Constructors:
+    public Exercise1() {
+        this.id = 0;
+        this.name = "default";
+        this.address = "default";
+        this.dob = "YYYY/MM/DD";
+        this.numAlbums = 0;
+    }
+    public Exercise1(int id, String name, String address, String dob, int numAlbums) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.dob = dob;
+        this.numAlbums = numAlbums;
+    }
+
+    // Setters:
+    public void setID(int id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setAddress(String address) { this.address = address; }
+    public void setDOB(String dob) { this.dob = dob; }
+    public void setNumAlbums(int numAlbums) { this.numAlbums = numAlbums; }
+
+    // Getters:
+    public int getID() { return id; }
+    public String getName() { return name; }
+    public String getAddress() { return address; }
+    public String getDOB() { return dob; }
+    public int getNumAlbums() { return numAlbums; }
 }
--- Matthew_COMP228Lab1/src/ex1/Exercise1Driver.java
+++ Matthew_COMP228Lab1/src/ex1/Exercise1Driver.java
@@ -1,4 +1,42 @@
+//Matthew Elliott - 301424215
+//September 18, 2025
+// Lab #1
+
 package ex1;
 
 public class Exercise1Driver {
+    public static void main(String[] args) {
+        //Initial singer object declaration:
+        Exercise1 singer1 = new Exercise1();
+
+        System.out.println("The default ID is: " + singer1.getID());
+        System.out.println("The default name is: " + singer1.getName());
+        System.out.println("The default address is: " + singer1.getAddress());
+        System.out.println("The default date of birth is: " + singer1.getDOB());
+        System.out.println("The default number of published albums is: " + singer1.getNumAlbums());
+        System.out.println();
+
+        //New singer object with set values:
+        Exercise1 singer2 = new Exercise1(123456789, "Paul McCartney", "7 Cavendish Avenue", "1942/06/18", 26);
+
+        System.out.println("The singer's ID is: " + singer2.getID());
+        System.out.println("The singer's name is: " + singer2.getName());
+        System.out.println(singer2.getName() + "'s address is: " + singer2.getAddress());
+        System.out.println(singer2.getName() + "'s birth date is: " + singer2.getDOB());
+        System.out.println(singer2.getName() + " has " + singer2.getNumAlbums() + "studio albums.");
+        System.out.println();
+
+        //Using setters to edit the values, then getting them again:
+        singer2.setID(987654321);
+        singer2.setName("Michael Jackson");
+        singer2.setAddress("Neverland Ranch");
+        singer2.setDOB("1958/08/29");
+        singer2.setNumAlbums(10);
+
+        System.out.println("The singer's ID is: " + singer2.getID());
+        System.out.println("The singer's name is: " + singer2.getName());
+        System.out.println(singer2.getName() + "'s address is: " + singer2.getAddress());
+        System.out.println(singer2.getName() + "'s birth date is: " + singer2.getDOB());
+        System.out.println(singer2.getName() + " has " + singer2.getNumAlbums() + "studio albums.");
+    }
 }
